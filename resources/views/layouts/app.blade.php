24160110c82870cec758d4d5cccc572076bd7b3b
Victor Dieppa Garriga <victor-dg@lightflows.co.uk>
2022-11-07 16:43:47 +0000

    Create static pages and navigation highlight

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link{{ Request::is('/') ? ' active' : '' }}" href="/">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link{{ Request::is('info') ? ' active' : '' }}" href="/info">{{ __('Info') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
