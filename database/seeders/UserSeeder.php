f2aaf93dc11277651252c7ab507c4934540f62fa
Victor Dieppa Garriga <victor-dg@lightflows.co.uk>
2022-11-10 11:06:54 +0000

    Use each() to seed the linked database tables

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hobby;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +17,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(100)->create();
+        User::factory()->count(100)->create()
+        ->each(function ($user) {
+            Hobby::factory()->count(rand(1,8))->create(
+                [
+                    'user_id' => $user->id,
+                ]
+            )
+            ->each( function ($hobby) {
+                $tag_id = range(1,8);
+                shuffle($tag_id);
+                $assignments = array_slice($tag_id, 0, rand(0,8)); // eg. 5,2,8
+                foreach ($assignments as $tag_id) {
+                    DB::table('hobby_tag')->insert(
+                        [
+                            'hobby_id' => $hobby->id,
+                            'tag_id' => $tag_id,
+                            'created_at' => Now(), // DB::table will not create created_at and updated_at
+                            'updated_at' => Now(), // automatically, like Model::factory()->create(} does
+                        ]
+                    );
+                }
+            });
+        });
     }
 }
