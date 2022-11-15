9e1d812056c678a9b781dbdafa28a8fceaffee4d
Victor Dieppa Garriga <victor-dg@lightflows.co.uk>
2022-11-15 14:09:01 +0000

    Apply Auth() middleware to the rest of the controllers

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -8,6 +8,11 @@ use App\Models\Tag;
 
 class TagController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+    }
+
     /**
      * Display a listing of the resource.
      *
