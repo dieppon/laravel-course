65a7dcb3c045d8d7e75a038f4e44f67183fd29f8
Victor Dieppa Garriga <victor-dg@lightflows.co.uk>
2022-11-15 12:57:37 +0000

    Control guests with the Auth() middleware

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -13,7 +13,7 @@ class HobbyController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except('index', 'show');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
